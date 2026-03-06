89acd2e3ff93082470bd2d6b6b95c4ece7dc9ae8
Abanoub_Mamdouh <abanoubmamdouh126@gmail.com>
2026-03-06 19:00:51 +0200

    Add files via upload

--- Back-End/Data/Models/Customer.cs
+++ Back-End/Data/Models/Customer.cs
@@ -1,1 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace Sa3dny.Data.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id_Customer { get; set; }
+        [Required]
+        [DisplayName("Name")]
+        public string Name_Customer { get; set; }
+        [Required]
+        [DisplayName("Address")]
+        public string Address_Customer { get; set; }
+        [Required]
+        [DisplayName("Phone")]
+        public string phone_Customer { get; set; }
+ 
+        public DateTime? created_at { get; set; }
+
+
+    }
+}
---
+++ Back-End/Data/Models/Provider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sa3dny.Data.Models
+{
+    public class Provider
+    {
+        [Key]
+        public int provider_id { get; set; }
+        [Required]
+        [DisplayName("Name")]
+       public string name_Provider { get; set; }
+        [Required]
+        [DisplayName("Address")]
+        public string address_Provider { get; set; }
+        [Required]
+        [DisplayName("National_ID")]
+        [StringLength(14)]
+        public int national_id_Provider { get; set; }
+        [DisplayName("Rate")]
+        public double? rate_Provider { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
