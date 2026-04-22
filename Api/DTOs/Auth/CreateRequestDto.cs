54af77df882afca70378e0c1d1bcbeee8b3410f3
Abanoub_Mamdouh <abanoubmamdouh126@gmail.com>
2026-04-23 01:55:37 +0200

    Add files via upload

---
+++ Api/DTOs/Auth/CreateRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sa3dny.Api.DTOs.Requests
+{
+    public class CreateRequestDto
+    {
+        [Required]
+        public int Customer_Id { get; set; }
+
+        [Required]
+        public int Service_Id { get; set; }
+
+        [Required]
+        public string Description_Req { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+    }
+}
---
+++ Api/DTOs/Auth/RequestResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sa3dny.Api.DTOs.Requests
+{
+    public class RequestResponseDto
+    {
+        public int Request_Id { get; set; }
+        public string Status { get; set; }
+
+    }
+}
